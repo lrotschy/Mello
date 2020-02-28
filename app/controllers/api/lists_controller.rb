a54b5000f8177b34a1049b3556b6cdd52a1a50d5
Holden Hinkle <holdenhinkle@Holdens-MacBook-Pro.local>
2020-02-28 13:24:49 -0500

    create update route and controller method

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -22,9 +22,28 @@ class Api::ListsController < ApplicationController
       render 'api/shared/error', status: 404      
   end
 
+  def update
+    @list = List.find(params[:id])
+    @list.update(update_list_params)
+    render :update, status: :ok
+
+  rescue ActionController::ParameterMissing
+    @error = "Invalid list data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+
+  rescue ActiveRecord::RecordNotFound
+    @error = "Record not found"
+    render 'api/shared/error', status: 404    
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:title)
+  end
+
+  def update_list_params
+    params.require(:title)
+    params.permit(:title)
   end
 end
