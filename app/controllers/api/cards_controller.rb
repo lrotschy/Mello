4bc6a6f8a7a208ce3be67fef3265fd7f5683d25e
Liela Rotschy <lielarotschy@gmail.com>
2020-03-03 13:05:38 -0500

    create card on backend

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -7,5 +7,33 @@ class Api::CardsController < ApplicationController
     render 'api/shared/error', status: 404
   end
 
+  def create 
+    
+    list = List.find(params[:list_id])
 
+    @card = Card.new(card_params) 
+
+    @card.list_id = list.id
+
+    if @card.save 
+      render :create, status: :created
+    else 
+      @error = @card.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end 
+
+  rescue ActionController::ParameterMissing
+    @error = "Invalid card data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+
+  rescue ActiveRecord::RecordNotFound
+    @error = "Record not found"
+    render 'api/shared/error', status: 404   
+  end 
+
+  private 
+
+  def card_params 
+    params.require(:card).permit(:title)
+  end
 end
