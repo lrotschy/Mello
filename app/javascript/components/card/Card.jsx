6a0837e17fe9c28a3eb45a86aaf6ee77ee5d7787
Liela Rotschy <lielarotschy@gmail.com>
2020-03-06 14:14:54 -0500

    Fix Redirect to Link Component

--- app/javascript/components/card/Card.jsx
+++ app/javascript/components/card/Card.jsx
@@ -1,48 +1,33 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Card extends Component {
-  state = {
-    redirect: false
-  };
-
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    });
-  };
-
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to={`/cards/${this.props.id}`} />;
-    }
-  };
-
   render() {
-    const { labels, title, dueDate, description } = this.props;
+    const { labels, title, dueDate, description, id } = this.props;
 
     return (
       <div className="card-background">
-        {this.renderRedirect()}
-        <div className="card " onClick={this.setRedirect}>
-          <i className="edit-toggle edit-icon sm-icon"></i>
-          <div className="card-info">
-            {labels.map((label, i) => (
-              <div
-                key={i}
-                className={`card-label ${label} colorblindable`}
-              ></div>
-            ))}
-            <p>{title}</p>
+        <Link to={`/cards/${id}`}>
+          <div className="card ">
+            <i className="edit-toggle edit-icon sm-icon"></i>
+            <div className="card-info">
+              {labels.map((label, i) => (
+                <div
+                  key={i}
+                  className={`card-label ${label} colorblindable`}
+                ></div>
+              ))}
+              <p>{title}</p>
+            </div>
+            <div className="card-icons">
+              <i className="clock-icon sm-icon overdue-recent completed">
+                {dueDate}
+              </i>
+              {description && <i className="description-icon sm-icon"></i>}
+              <i className="comment-icon sm-icon"></i>
+            </div>
           </div>
-          <div className="card-icons">
-            <i className="clock-icon sm-icon overdue-recent completed">
-              {dueDate}
-            </i>
-            {description && <i className="description-icon sm-icon"></i>}
-            <i className="comment-icon sm-icon"></i>
-          </div>
-        </div>
+        </Link>
       </div>
     );
   }
